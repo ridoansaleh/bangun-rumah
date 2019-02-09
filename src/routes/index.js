e36c138629f82085872988b5527f56cc3294d7d6
RSN <ridoansalehnasution98@gmail.com>
2019-02-09 23:10:47 +0700

    Routes (#2)

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './Home/HomeView';
+import CategoryScreen from './Category/CategoryView';
+import ShoppingCartScreen from './ShoppingCart/ShoppingCartView';
+import ProfileScreen from './Profile/ProfileView';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Category: CategoryScreen,
+    ShoppingCart: ShoppingCartScreen,
+    Profile: ProfileScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default createAppContainer(AppNavigator);
