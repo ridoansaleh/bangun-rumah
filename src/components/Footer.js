9bd6352065dc7fcc57c4d24b207df5322cab20bb
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-02-11 00:06:33 +0700

    add progress of home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
+import { Footer, FooterTab, Button, Icon } from 'native-base';
 
 class BottomNavbar extends Component {
   render() {
     return (
       <Footer>
         <FooterTab>
-          <Button vertical active>
-            <Icon name="home" />
+          <Button vertical>
+            <Icon active name="home" />
           </Button>
           <Button vertical>
             <Icon name="apps" />
           </Button>
           <Button vertical>
-            <Icon active name="cart" />
+            <Icon name="cart" />
           </Button>
           <Button vertical>
             <Icon name="person" />
