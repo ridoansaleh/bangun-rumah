620241a969a006b984c73e967da3a04b2ad4b4e7
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-02-12 05:51:32 +0700

    complete login page

--- src/routes/Login/LoginView.js
+++ src/routes/Login/LoginView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Image, Dimensions } from 'react-native';
+import { StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
 import {
   Button,
   Container,
@@ -11,19 +11,26 @@ import {
   Body,
   Right,
   Title,
-  View,
   Form,
   Item,
   Input,
+  Text,
   Label,
+  View,
 } from 'native-base';
+import { Grid, Col } from 'react-native-easy-grid';
 const loginUser = require('../../../assets/login-user.jpg');
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
+// Forgot password link is not ready yet
 
 class LoginView extends Component {
   static propTypes = {
     openDrawer: PropTypes.func,
+  };
+
+  handleRouteChange = url => {
+    this.props.navigation.navigate(url);
   };
 
   render() {
@@ -41,7 +48,7 @@ class LoginView extends Component {
           </Body>
           <Right />
         </Header>
-        <Content padder contentContainerStyle={styles.wrapper}>
+        <Content padder style={styles.contentContainer}>
           <Image source={loginUser} style={styles.image} />
           <Form>
             <Item floatingLabel>
@@ -52,7 +59,28 @@ class LoginView extends Component {
               <Label>Password</Label>
               <Input />
             </Item>
+            <Grid style={styles.btnBox}>
+              <Col style={styles.block}>
+                <Button
+                  danger
+                  style={styles.btn}
+                  onPress={() => this.handleRouteChange('Register')}>
+                  <Text>Daftar</Text>
+                </Button>
+              </Col>
+              <Col style={styles.block}>
+                <Button success style={styles.btn}>
+                  <Text>Login</Text>
+                </Button>
+              </Col>
+            </Grid>
           </Form>
+          <View style={styles.forgotPassword}>
+            <Text>Anda lupa password? Klik di </Text>
+            <TouchableOpacity onPress={() => this.handleRouteChange('Home')}>
+              <Text>sini</Text>
+            </TouchableOpacity>
+          </View>
         </Content>
       </Container>
     );
@@ -63,13 +91,31 @@ const styles = StyleSheet.create({
   header: {
     marginTop: 25,
   },
-  wrapper: {
-    alignItems: 'center',
-    height: 300,
+  contentContainer: {
+    marginTop: height * 0.1,
   },
   image: {
     width: width * 0.25,
     height: width * 0.25,
+    marginLeft: (width * 0.75) / 2,
+    marginRight: (width * 0.75) / 2,
+  },
+  btnBox: {
+    marginTop: 30,
+  },
+  block: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    width: width * 0.4,
+    justifyContent: 'center',
+  },
+  forgotPassword: {
+    marginTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
 
