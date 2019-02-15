386c86b98c0e53c3d2790044375e215cb1403711
RSN <ridoansalehnasution98@gmail.com>
2019-02-15 14:24:19 +0700

    add category product page (#13)

--- src/routes/ShoppingCart/ShoppingCartView.js
+++ src/routes/ShoppingCart/ShoppingCartView.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Container, Content, Text } from 'native-base';
+import { Row, Grid } from 'react-native-easy-grid';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Authentication from '../../components/Authentication';
 
 class ShoppingCartScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
-      </View>
+      <Container>
+        <Header openDrawer={() => this.props.nav.navigation.openDrawer()} />
+        <Content>
+          <Text>Shopping Cart</Text>
+        </Content>
+        <Footer />
+      </Container>
     );
   }
 }
 
-export default ShoppingCartScreen;
+export default Authentication(ShoppingCartScreen);
