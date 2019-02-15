3472b1af4c17096e348480d4cd45c67789fcfa8c
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-02-15 18:55:04 +0700

    add shopping cart page

--- src/routes/ShoppingCart/ShoppingCartView.js
+++ src/routes/ShoppingCart/ShoppingCartView.js
@@ -1,18 +1,263 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Container, Content, Text } from 'native-base';
-import { Row, Grid } from 'react-native-easy-grid';
+import { Image, Dimensions, ScrollView } from 'react-native';
+import { Container, Content, Text, CheckBox, Button } from 'native-base';
+import { Row, Grid, Col } from 'react-native-easy-grid';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Authentication from '../../components/Authentication';
+
+const { width, height } = Dimensions.get('window');
+const defaultImage = require('../../../assets/default-product.jpg');
 
 class ShoppingCartScreen extends Component {
   render() {
     return (
       <Container>
         <Header openDrawer={() => this.props.nav.navigation.openDrawer()} />
-        <Content>
-          <Text>Shopping Cart</Text>
+        <Content style={{ backgroundColor: 'yellow' }}>
+          <Grid style={{ backgroundColor: 'white', height: height * 0.8 }}>
+            <Row size={10}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold', padding: 10 }}>Keranjang</Text>
+            </Row>
+            <Row size={80}>
+              <ScrollView>
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+
+                <Row style={{ marginBottom: 10 }}>
+                  <Col size={1}>
+                    <CheckBox checked={false} style={{ marginTop: width * 0.2 * 0.25 }} />
+                  </Col>
+                  <Col size={3}>
+                    <Image
+                      source={defaultImage}
+                      style={{ width: width * 0.2, height: height * 0.1 }}
+                    />
+                  </Col>
+                  <Col size={4} style={{ paddingTop: width * 0.2 * 0.1 }}>
+                    <Text>Nama Produk</Text>
+                    <Text>Rp 12.000</Text>
+                  </Col>
+                  <Col
+                    size={2}
+                    style={{
+                      marginTop: width * 0.2 * 0.2,
+                      textAlign: 'right',
+                    }}>
+                    <Text>Satuan</Text>
+                  </Col>
+                </Row>
+              </ScrollView>
+            </Row>
+            <Row
+              size={10}
+              style={{
+                backgroundColor: 'white',
+                borderColor: 'black',
+                borderTopWidth: 1,
+              }}>
+              <Col size={1}>
+                <CheckBox checked={false} style={{ marginTop: 15 }} />
+              </Col>
+              <Col size={7}>
+                <Text style={{ marginTop: 15 }}>Semua</Text>
+              </Col>
+              <Col size={2}>
+                <Button small bordered dark style={{ marginTop: 10 }}>
+                  <Text>Pesan</Text>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
         </Content>
         <Footer />
       </Container>
