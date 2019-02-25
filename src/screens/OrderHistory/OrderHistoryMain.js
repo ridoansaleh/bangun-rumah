52501ce29f134df9eec56df698de0afbf3fac94e
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-02-25 20:35:11 +0700

    integrate order history page with firebase

--- src/screens/OrderHistory/OrderHistoryMain.js
+++ src/screens/OrderHistory/OrderHistoryMain.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Alert, AsyncStorage, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, Dimensions } from 'react-native';
 import {
   Button,
   Container,
@@ -11,25 +11,55 @@ import {
   Body,
   Right,
   Title,
-  Form,
-  Item,
-  Input,
+  Spinner,
   Text,
-  Label,
   View,
 } from 'native-base';
-import { Grid, Col } from 'react-native-easy-grid';
+import { Grid, Col, Row } from 'react-native-easy-grid';
 import Authentication from '../../components/Authentication';
-import loginUser from '../../../assets/login-user.jpg';
-import { auth as authenticate, db } from '../../../firebase.config';
+import sadImage from '../../../assets/sad_face.png';
+import { db } from '../../../firebase.config';
 import { urls } from '../../constant';
+import { convertToCurrency } from '../../utils';
 
 const { width, height } = Dimensions.get('window');
-// Forgot password link is not ready yet
 
 class OrderHistoryScreen extends Component {
   static propTypes = {
     nav: PropTypes.object,
+    user: PropTypes.object,
+  };
+
+  state = {
+    isDataFetched: false,
+    dataOrders: [],
+  };
+
+  componentDidMount() {
+    this.getOrderHistoryData();
+  }
+
+  getOrderHistoryData = () => {
+    let orders = [];
+    db.collection('pemesanan')
+      .where('id_user', '==', this.props.user.id)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          orders.push(doc.data());
+        });
+        this.setState({
+          isDataFetched: true,
+          dataOrders: orders,
+        });
+      })
+      .catch(error => {
+        console.error("Error getting order history's data \n", error);
+      });
+  };
+
+  printOrderNote = () => {
+    // print order note
   };
 
   render() {
@@ -46,8 +76,81 @@ class OrderHistoryScreen extends Component {
           </Body>
           <Right />
         </Header>
-        <Content>
-          <Text>Riwayat Pemesanan</Text>
+        <Content padder>
+          {!this.state.isDataFetched && (
+            <View style={styles.spin}>
+              <Spinner color="green" size="large" />
+            </View>
+          )}
+          {this.state.isDataFetched && this.state.dataOrders.length > 0 && (
+            <View style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}>
+              {this.state.dataOrders.map((data, i) => (
+                <Grid key={i}>
+                  <Row>
+                    <Col>
+                      <Image
+                        source={{ uri: data.photo_produk }}
+                        style={{ width: 0.4 * width, height: 0.25 * height }}
+                      />
+                    </Col>
+                    <Col>
+                      <Text style={{ fontWeight: 'bold' }}>{data.nama_produk}</Text>
+                      <Text>({data.nama_toko})</Text>
+                      <Text style={{ fontSize: 12 }}>{data.waktu_pemesanan}</Text>
+                      <Text style={{ marginTop: 25 }}>
+                        Rp {convertToCurrency(data.harga)} / {data.jumlah_produk} {data.satuan}
+                      </Text>
+                      <Text>{data.status}</Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col size={4} />
+                    <Col size={6}>
+                      <Button
+                        small
+                        bordered
+                        style={{ marginTop: 15 }}
+                        onClick={() => this.printOrderNote()}>
+                        <Text>Cetak Bukti Pemesanan</Text>
+                      </Button>
+                    </Col>
+                  </Row>
+                </Grid>
+              ))}
+            </View>
+          )}
+          {this.state.isDataFetched && this.state.dataOrders.length === 0 && (
+            <View
+              style={{
+                flex: 1,
+                height: 0.4 * height,
+                marginTop: 0.2 * height,
+                marginBottom: 0.2 * height,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={sadImage}
+                style={{
+                  width: 0.35 * width,
+                  height: 0.2 * height,
+                }}
+              />
+              <Text style={{ marginTop: 15 }}>Anda belum memiliki riwayat pemesanan</Text>
+              <Button
+                small
+                bordered
+                style={{
+                  width: 0.25 * width,
+                  marginLeft: 0.375 * width,
+                  marginRight: 0.375 * width,
+                  marginTop: 20,
+                }}
+                onPress={() => this.props.nav.navigation.navigate(urls.home)}>
+                <Text>Belanja</Text>
+              </Button>
+            </View>
+          )}
         </Content>
       </Container>
     );
@@ -57,6 +160,15 @@ class OrderHistoryScreen extends Component {
 const styles = StyleSheet.create({
   header: {
     marginTop: 25,
+  },
+  spin: {
+    paddingVertical: 6,
+    width: width * 0.25,
+    height: height * 0.25,
+    marginLeft: (width * 0.75) / 2,
+    marginRight: (width * 0.75) / 2,
+    marginTop: (height * 0.75) / 2,
+    marginBottom: (height * 0.75) / 2,
   },
 });
 
